ab20f40e13ef4fa06a246125c5f677dc8c2b5680
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-07-03 12:15:26 +0200

    made user model and added default values for user avatar

--- src/app/user-avatar/user-avatar.component.ts
+++ src/app/user-avatar/user-avatar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/user';
 
 @Component({
   selector: 'app-user-avatar',
   templateUrl: './user-avatar.component.html',
   styleUrls: ['./user-avatar.component.scss']
 })
-export class UserAvatarComponent {
+export class UserAvatarComponent implements OnInit{
+  user: User = {
+    name: "Jhon Smith",
+    role: "Super Admin",
+    email: "test@gmail.com",
+    avatarUrl: "../../assets/images/user-icon.jpg"
+  }
 
+  constructor() {}
+
+  ngOnInit(): void {  }
 }
