aa1c438c1f22b8b6dd0ab1a3032bb318ad282139
Katya <katyanikitina16@gmail.com>
2023-07-05 10:55:35 +0200

    Deleted unused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserAvatarComponent } from './user-avatar/user-avatar.component';
-import { SingInComponent } from './sing-in/sing-in.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserAvatarComponent,
-    SingInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
