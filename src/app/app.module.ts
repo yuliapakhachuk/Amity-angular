aa56cdb200404c9a14d8fd031a120bdd378c7b77
Katya <katyanikitina16@gmail.com>
2023-07-05 11:43:10 +0200

    Created sign-in-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserAvatarComponent,
+    SignInPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
