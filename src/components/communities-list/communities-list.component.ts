2031c2a0c34412d56dbb52a1671d333ca08f9178
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-07-07 16:14:28 +0200

    fix conflicts

---
+++ src/components/communities-list/communities-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-communities-list',
+  templateUrl: './communities-list.component.html',
+  styleUrls: ['./communities-list.component.scss']
+})
+export class CommunitiesListComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
