3759040957184cc15f272c7ac8fa7913cef4edc4
Katya <katyanikitina16@gmail.com>
2023-07-18 16:45:37 +0200

    Created styles for checkbox

--- src/app/components/sign-in-page/sign-in-page.component.ts
+++ src/app/components/sign-in-page/sign-in-page.component.ts
@@ -13,7 +13,8 @@ export class SignInPageComponent {
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required])
+      password: new FormControl(null, [Validators.required]),
+      rememberUser: new FormControl(false)
     })
   }
 
