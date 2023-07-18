42c75ea28230e9c4a681023b5f5336683eb5fca2
Katya <katyanikitina16@gmail.com>
2023-07-18 13:04:36 +0200

    Created interfaces

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,35 @@
+export type UserRole = "administrator" | "supervisor" | "coordinator" | "observer" | "resident"; 
+
+export interface Building {
+    name: string,
+    address: string,
+    state: string,
+    coordinatorId: number, ///????
+    safetyOn: boolean,
+}
 export interface User {
+    id: number,
     firstName: string,
     lastName: string,
-    role: string,
+    role: UserRole,
     email: string,
     phone: string,
-    avatarUrl: string,
+    avatarUrl?: string,
+    property?: string,
+}
+
+export interface Community {
+    id: number,
+    name: string,
+    state: string,
+    address: string,
+    zipCode: string,
+    contactData: string,
+    safetyOn: boolean,   
+    description?: string,
+    image?: string,
+    supervisor?: User,  //??? supervisorId: number ///???
+    buildings?: Building[],
+    membersId?: number[]  ///??? buildingsId?: number[]
+    //history ?????
 }
