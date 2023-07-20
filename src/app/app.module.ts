d89a64067c4c77f30331de5d1db91683aec1693c
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-07-20 09:59:28 +0200

    member list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShowPasswordEyeComponent } from './components/show-password-eye/show-password-eye.component';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
 import { CommunitiesListComponent } from './components/communities-list/communities-list.component';
-import { MembersListComponent } from '../components/members-list/members-list.component';
+import { MembersListComponent } from './components/members-list/members-list.component';
 import { RowIsPressedDirective } from './sharing/directives/row-is-pressed.directive';
 import { LoginMainPageComponent } from './components/login-main-page/login-main-page.component';
 
